75f7e9e2eb6ff350f39406647d92c4a64f9554d2
Kelly Bristol <bristolkr@gmail.com>
2015-06-02 16:01:09 -0400

    ready to deploy

--- app/models/life_event.rb
+++ app/models/life_event.rb
@@ -1,7 +1,7 @@
 class LifeEvent < ActiveRecord::Base
-  attr_accessor :name, :date, :description
+  # attr_accessor :name, :date, :description
 
-  validates :name, :presence => true
-  validates :date, :presence => true
+  # validates :name, :presence => true
+  # validates :date, :presence => true
 
 end
