2f5fa531adf8c92196f0ee56a0ba1169974b717e
Kelly Bristol <bristolkr@gmail.com>
2015-06-05 14:50:05 -0400

    haaaaaaaaaaaaaands

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :life_events
   resources :schools
+  resources :person
   root :to => "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
